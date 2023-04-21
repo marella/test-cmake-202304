48ce25a56510f987a118153db2b1470c6b0ab8ab
Ravindra Marella <marella@users.noreply.github.com>
2023-04-22 02:38:26 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,22 +1,126 @@
 #include <stdio.h>
 #include <string>
 
+int ggml_cpu_has_avx(void)
+{
+#if defined(__AVX__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_avx2(void)
+{
+#if defined(__AVX2__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_avx512(void)
+{
+#if defined(__AVX512F__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_fma(void)
+{
+#if defined(__FMA__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_neon(void)
+{
+#if defined(__ARM_NEON)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_arm_fma(void)
+{
+#if defined(__ARM_FEATURE_FMA)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_f16c(void)
+{
+#if defined(__F16C__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_fp16_va(void)
+{
+#if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_wasm_simd(void)
+{
+#if defined(__wasm_simd128__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_blas(void)
+{
+#if defined(GGML_USE_ACCELERATE) || defined(GGML_USE_OPENBLAS)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_sse3(void)
+{
+#if defined(__SSE3__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int ggml_cpu_has_vsx(void)
+{
+#if defined(__POWER9_VECTOR__)
+  return 1;
+#else
+  return 0;
+#endif
+}
+
 int main(int argc, char *argv[])
 {
-  static bool avx = __builtin_cpu_supports("avx");
-  static bool avx2 = __builtin_cpu_supports("avx2");
-  static bool avx512f = __builtin_cpu_supports("avx512f");
-  static bool avx512vbmi = __builtin_cpu_supports("avx512vbmi");
-  static bool avx512vnni = __builtin_cpu_supports("avx512vnni");
-  static bool fma = __builtin_cpu_supports("fma");
-  static bool sse3 = __builtin_cpu_supports("sse3");
+  static int avx = ggml_cpu_has_avx();
+  static int avx2 = ggml_cpu_has_avx2();
+  static int avx512 = ggml_cpu_has_avx512();
+  static int fma = ggml_cpu_has_fma();
+  static int sse3 = ggml_cpu_has_sse3();
   static std::string s;
   s = "My hardware supports:\n";
   s += "AVX = " + std::to_string(avx) + "\n";
   s += "AVX2 = " + std::to_string(avx2) + "\n";
-  s += "AVX512 = " + std::to_string(avx512f) + "\n";
-  s += "AVX512_VBMI = " + std::to_string(avx512vbmi) + "\n";
-  s += "AVX512_VNNI = " + std::to_string(avx512vnni) + "\n";
+  s += "AVX512 = " + std::to_string(avx512) + "\n";
   s += "FMA = " + std::to_string(fma) + "\n";
   s += "SSE3 = " + std::to_string(sse3) + "\n";
   printf("%s", s.c_str());
