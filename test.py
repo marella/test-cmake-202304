da5dea32a12bfb0929c6a4f9294acaffe7b20e4a
Ravindra Marella <marella@users.noreply.github.com>
2023-04-22 22:27:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from gpt4allj import Model, load_library
 
-# lib = load_library('./lib/libgptj.dylib', './lib/libggml.dylib')
+lib = load_library('./lib/avx/libgptj.dylib', './lib/avx/libggml.dylib')
 
-model = Model('./ggml-gpt4all-j.bin')
+model = Model('./ggml-gpt4all-j.bin', lib=lib)
 
 print(model.generate('AI is going to', n_predict=2))
