ee11d290c5b21412373d0dace7735955311317df
Ravindra Marella <marella@users.noreply.github.com>
2023-04-22 22:29:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from gpt4allj import Model, load_library
 
-lib = load_library('./lib/avx/libgptj.dylib', './lib/avx/libggml.dylib')
+lib = load_library('./lib/avx2/libgptj.dylib', './lib/avx2/libggml.dylib')
 
 model = Model('./ggml-gpt4all-j.bin', lib=lib)
 
